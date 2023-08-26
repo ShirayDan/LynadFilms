f0a77ac6f29f675d8a414a9bed154f83eae83343
ShirayDan <dan.shirayy@gmail.com>
2023-08-26 10:22:30 +0300

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.scss'
+
+function App() {
+	return (
+		<>
+			<div className={`text-3xl font-bold underline ${styles.rrrr}`}>Test</div>
+		</>
+	)
+}
+
+export default App
